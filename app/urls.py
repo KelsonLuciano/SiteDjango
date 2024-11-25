8eba6f79fee0edfbb5102fe58cc25724b35cb627
KelsonLuciano <kelsonluciano163@gmail.com>
2024-11-25 09:36:47 -0300

    Upload e associação de imagens

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -9,6 +10,8 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from uploader.router import router as uploader_router
 
 from core.views import UserViewSet
 from core.views import CategoriaViewSet
@@ -37,6 +40,7 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
+    path("api/media/", include(uploader_router.urls)),
     # Simple JWT
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
@@ -44,3 +48,4 @@ urlpatterns = [
     path("api/", include(router.urls)),
 ]
 
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
